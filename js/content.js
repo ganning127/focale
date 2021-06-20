179ce404cc997c832883622164f1460fcd35f02d
Ganning Xu <ganningxu@gmail.com>
2021-06-19 21:54:52 -0400

    manifest update

--- js/content.js
+++ js/content.js
@@ -1,32 +1,15 @@
 let running;
-findAllURL = function (urls) {
-  var current = window.location.href;
-  let indexBlock;
-  for (let i = 0; i < urls.length; i++) {
-    if (current.startsWith(urls[i])) {
-      document.documentElement.innerHTML = `Come on.... no going on ${urls[i]} while you are studying!!`;
-    }
-  }
-};
+blockSite = function () {
+  var url = window.location.href;
+      document.documentElement.innerHTML = `Come on.... no going on ${url} while you are studying!!`;
+  };
 
 chrome.storage.local.get(["running"], function (result) {
   running = result.running;
   chrome.storage.local.get(["siteBlocking"], function (result) {
     // console.log("BLOCKING: " + result.siteBlocking)
     if (result.siteBlocking) {
-      console.log("RNNING2: " + running);
-      if (running) {
-        findAllURL([
-          "https://www.instagram.com/",
-          "https://www.facebook.com/",
-          "https://www.reddit.com/",
-          "https://www.whatsapp.com/",
-          "https://www.messenger.com/",
-          "https://www.wechat.com/",
-          "https://www.tumblr.com/",
-          "https://www.pinterest.com/",
-        ]);
-      }
+      blockSite()
     } else {
       console.log("TURN SITE BLOCKING ON LOL NO CHEATING");
     }
